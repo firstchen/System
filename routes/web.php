a2e982822f1816c5c9b79abdbba02a1209193f0f
陈勇 <chenyong@xmpaoyou.com>
2021-03-20 18:28:35 +0800

    Route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,38 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+// 用户相关
+$router->group(['namespace' => 'User'], function () use ($router) {
+
+});
+
+// 客户相关
+$router->group(['namespace' => 'Customer'], function () use ($router) {
+
+});
+
+// 销售相关
+$router->group(['namespace' => 'Sale'], function () use ($router) {
+
+});
+
+// 产品相关
+$router->group(['namespace' => 'Product'], function () use ($router) {
+
+});
+
+// 财务相关
+$router->group(['namespace' => 'Finance'], function () use ($router) {
+
+});
+
+// 审批体系相关
+$router->group(['namespace' => 'Approve'], function () use ($router) {
+
+});
+
+// 渠道合作商相关
+$router->group(['namespace' => 'Channel'], function () use ($router) {
+
+});
